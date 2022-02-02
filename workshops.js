7c343a41512ff9960c7e6e0a6d3ae95b2a4a3bd1
Simon Wiles <simonjwiles@gmail.com>
2022-02-02 14:41:14 -0800

    Update for new Stanford Events Calendar RSS feed

--- workshops.js
+++ workshops.js
@@ -1,4 +1,5 @@
-const RSS_URL = "sul-events-feed.rss.xml";
+const RSS_URL =
+  "https://events.stanford.edu/widget/view?schools=stanford&departments=stanford_university_libraries&days=90&num=50&html_descriptions=1&hide_past=1&format=rss";
 
 const reEscapedHtml = /&(?:amp|lt|gt|quot|#39|#96);/g,
   reHasEscapedHtml = RegExp(reEscapedHtml.source),
@@ -13,7 +14,39 @@ const reEscapedHtml = /&(?:amp|lt|gt|quot|#39|#96);/g,
   unescape = (string) =>
     string && reHasEscapedHtml.test(string)
       ? string.replace(reEscapedHtml, (x) => htmlUnescapes[x])
-      : string;
+      : string,
+  days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ],
+  months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ],
+  formatDate = (date) => {
+    date = new Date(date);
+    return `${days[date.getDay()]}, ${
+      months[date.getMonth()]
+    } ${date.getDate()}, ${date.getHours().toString().padStart(2, "0")}:${date
+      .getMinutes()
+      .toString()
+      .padStart(2, "0")}`;
+  };
 
 fetch(RSS_URL)
   .then((response) => response.text())
@@ -22,18 +55,15 @@ fetch(RSS_URL)
     const items = data.querySelectorAll("item");
     let html = ``;
     Array.from(items)
-      .filter((el) =>
-        el
-          .querySelector("title")
-          .innerHTML.includes("CIDR"),
-      )
+      .filter((el) => el.querySelector("title").innerHTML.includes("CIDR"))
       .forEach((el) => {
         html += `
         <div class="workshop">
           <img src="${el
-            .querySelector("enclosure")
+            .querySelector("content")
             .getAttribute("url")
             .replace(/^http:/, "https:")}" alt="">
+          <div>
           <h3>
             <a href="${
               el.querySelector("link").innerHTML
@@ -41,10 +71,15 @@ fetch(RSS_URL)
               ${el.querySelector("title").innerHTML}
             </a>
           </h3>
+          <p>${formatDate(el.querySelector("pubDate").innerHTML)}</p>
+          <details>
+          <summary>details</summary>
           ${unescape(el.querySelector("description").innerHTML).replace(
             "\\",
             "",
           )}
+          </details>
+          </div>
         </div>
       `;
       });
